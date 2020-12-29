63b2570e99f22cbf20996a87603d2fba1ae489dd
minhphan81299 <minh.phan81299@gmail.com>
2020-12-29 10:52:56 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
