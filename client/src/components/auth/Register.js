c1a613c1bb2dd041159ef9c878ba0eaa83dc4be3
minhphan81299 <minh.phan81299@gmail.com>
2021-01-09 20:22:45 +0700

    add register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Register() {
-  return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	});
+	const { name, email, password, password2 } = formData;
+	const onChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		console.log('submit');
+		const newUser = {
+			name,
+			email,
+			password,
+		};
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			};
+			const body = JSON.stringify(newUser);
+			const res = await axios.post('/api/users', body, config);
+			console.log(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<>
+			<h1 className='large text-primary'>Sign Up</h1>
+			<p className='lead'>
+				<i className='fas fa-user' /> Create Your Account
+			</p>
+			<form className='form' onSubmit={onSubmit}>
+				<div className='form-group'>
+					<input type='text' placeholder='Name' name='name' value={name} onChange={onChange} />
+				</div>
+				<div className='form-group'>
+					<input type='email' placeholder='Email Address' name='email' value={email} onChange={onChange} />
+					<small className='form-text'>
+						This site uses Gravatar so if you want a profile image, use a Gravatar email
+					</small>
+				</div>
+				<div className='form-group'>
+					<input type='password' placeholder='Password' name='password' value={password} onChange={onChange} />
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Confirm Password'
+						name='password2'
+						value={password2}
+						onChange={onChange}
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Register' />
+			</form>
+			<p className='my-1'>
+				Already have an account? <Link to='/login'>Sign In</Link>
+			</p>
+		</>
+	);
 }
 
 export default Register;
